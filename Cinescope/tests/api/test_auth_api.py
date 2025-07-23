e6d7b1ee76ae2d3d817be2e8b8e79ee2b5690d19
Marsel <marsel249@yahoo.com>
2025-07-23 19:42:54 +0300

    something commit

---
+++ Cinescope/tests/api/test_auth_api.py
@@ -0,0 +1,37 @@
+import pytest
+import requests
+# from constants import BASE_URL, HEADERS, REGISTER_ENDPOINT
+
+import sys
+from pathlib import Path
+
+# Путь к директории на два уровня выше
+root_dir = Path(__file__).resolve().parents[2]  # .parents[2] = два уровня вверх
+sys.path.append(str(root_dir))
+
+# Импорт модуля из этой директории
+from constants import BASE_URL, HEADERS, REGISTER_ENDPOINT
+from conftest import test_user, auth_session
+
+
+class TestAuthAPI:
+    def test_register_user(self, test_user):
+        # URL для регистрации
+        register_url = f"{BASE_URL}{REGISTER_ENDPOINT}"
+
+        # Отправка запроса на регистрацию
+        response = requests.post(register_url, json=test_user, headers=HEADERS)
+
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
+
+        # Проверки
+        assert response.status_code == 201, "Ошибка регистрации пользователя"
+        response_data = response.json()
+        assert response_data["email"] == test_user["email"], "Email не совпадает"
+        assert "id" in response_data, "ID пользователя отсутствует в ответе"
+        assert "roles" in response_data, "Роли пользователя отсутствуют в ответе"
+
+        # Проверяем, что роль USER назначена по умолчанию
+        assert "USER" in response_data["roles"], "Роль USER должна быть у пользователя"
